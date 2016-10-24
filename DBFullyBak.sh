b04cc2e8684d9faf57944c6ef38b24810f08c4b7
Joson <wucp@bp1010.com>
2016-10-24 18:54:01 +0800

    添加mysqldump脚本

---
+++ DBFullyBak.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Program
+#    use mysqldump to Fully backup mysql data per week!
+# 	 and Delete old Backup 
+# Path
+BakDir=/data/mysql/backup
+LogFile=/data/mysql/backup/bak.log
+Date=`date +%Y%m%d`
+Begin=`date +"%Y年%m月%d日 %H:%M:%S"`
+MYSQLDUMP="/usr/local/mysql/bin/mysqldump"
+DBUSER="root"
+DBPASSWD="Baipeng2016"
+DBNAME="mediaman messagecenter usercenter weiphp"
+
+
+DumpFile=$Date.sql
+GZDumpFile=$Date.sql.tgz
+
+cd $BakDir
+
+# $MYSQLDUMP -h 127.0.0.1 -u $DBUSER -p$DBPASSWD --quick --all-databases --flush-logs --delete-master-logs --single-transaction > $DumpFile
+$MYSQLDUMP -h 127.0.0.1 -u$DBUSER -p$DBPASSWD --quick --flush-logs --delete-master-logs --single-transaction --databases $DBNAME> $DumpFile
+tar czf $GZDumpFile $DumpFile
+rm $DumpFile
+Last=`date +"%Y年%m月%d日 %H:%M:%S"`
+echo 开始:$Begin 结束:$Last $GZDumpFile succ >> $LogFile
+#cd $BakDir/daily
+#rm -f *
